c0b9f56ecbf36e1bb9ee819b9586fd02a4db629e
Jonathan Allen <jallen@snow.edu>
2019-12-14 00:16:25 -0700

    Day 3 Part 1 complete.

--- day2/test/day1_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Day2Test do
-  use ExUnit.Case
-  doctest Day2
-
-  #test "greets the world" do
-    #assert Day1.hello() == :world
-  #end
-end
---
+++ day2/test/doctests.exs
@@ -0,0 +1,7 @@
+defmodule Day2Test do
+  use ExUnit.Case
+  doctest Day2
+
+  doctest Day3
+
+end
